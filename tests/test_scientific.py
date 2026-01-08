684c410343192b02d5df800b572738a2d1ce2b5e
Mohd Shariq <mohd.shariq@ignitarium.com>
2026-01-08 15:35:01 +0530

    Update code to have sanity test seperated

--- tests/test_scientific.py
+++ tests/test_scientific.py
@@ -1,9 +1,17 @@
 from calculator.scientific import power, sqrt
+import pytest
 
-
+@pytest.mark.sanity
 def test_power():
     assert power(2, 3) == 8
 
-
+@pytest.mark.sanity
 def test_sqrt():
     assert sqrt(9) == 3
+
+
+@pytest.mark.regression
+def test_power_reg():
+    print("Regression test case")
+    assert power(3, 2) == 8
+
