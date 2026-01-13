94b2ba254e6da6e647acbd81cb9cb9e3de84637e
Mohd Shariq <mohd.shariq@ignitarium.com>
2026-01-13 09:22:38 +0530

    update naming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="python-calculator-basic",
+    name="python-calculator-scientific",
     version="0.1.0",
     packages=find_packages(),
 )
